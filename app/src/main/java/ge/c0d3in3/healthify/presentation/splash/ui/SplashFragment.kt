89145d4c0dfcb6e8f82f3d399e819cd3b1df15b3
ted3x <tedo.manvelidze@space.ge>
2021-06-22 22:45:38 +0400

    rename splash_fragment.xml

--- app/src/main/java/ge/c0d3in3/healthify/presentation/splash/ui/SplashFragment.kt
+++ app/src/main/java/ge/c0d3in3/healthify/presentation/splash/ui/SplashFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.View
 import ge.c0d3in3.healthify.R
 import ge.c0d3in3.healthify.base.BaseFragment
-import ge.c0d3in3.healthify.databinding.FragmentSplashBinding
+import ge.c0d3in3.healthify.databinding.SplashFragmentBinding
 import ge.c0d3in3.healthify.presentation.splash.di.splashModule
 import ge.c0d3in3.healthify.presentation.splash.vm.SplashViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SplashFragment :
-    BaseFragment<FragmentSplashBinding, SplashViewModel>(FragmentSplashBinding::inflate) {
+    BaseFragment<SplashFragmentBinding, SplashViewModel>(SplashFragmentBinding::inflate) {
 
     override var module = splashModule
     override val vm by viewModel<SplashViewModel>()
