9ebbce05fb4b08d3049eb4cef05c43b345627ca4
ted3x <tedo.manvelidze@space.ge>
2021-06-24 20:30:47 +0400

    add alertMessage observer in BaseFragment.kt add extensions in BaseFragment.kt

--- app/src/main/java/ge/c0d3in3/healthify/base/BaseFragment.kt
+++ app/src/main/java/ge/c0d3in3/healthify/base/BaseFragment.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
+import ge.c0d3in3.healthify.MainActivity
+import ge.c0d3in3.healthify.R
 import ge.c0d3in3.healthify.utils.AlertDialog
+import org.koin.android.ext.android.bind
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
@@ -19,13 +23,15 @@ abstract class BaseFragment<VB : ViewBinding, VM : BaseViewModel>(
     private val inflate: Inflate<VB>
 ) : Fragment() {
 
-    protected abstract var module: Module
+    protected open val showBackButton: Boolean = false
+    protected open val toolbarTitleRes: Int? = null
+    protected abstract val module: Module
     protected abstract val vm: VM
     private var _binding: VB? = null
     protected val binding
         get() = if (_binding == null)
             throw IllegalStateException("${this::class.java.simpleName}'s binding is null")
-        else _binding
+        else _binding!!
 
     private var dialog: AlertDialog? = null
 
@@ -39,8 +45,30 @@ abstract class BaseFragment<VB : ViewBinding, VM : BaseViewModel>(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        with(requireActivity() as MainActivity) {
+            setBackButtonVisibility(showBackButton)
+            setToolbarTitle(toolbarTitleRes)
+        }
         _binding = inflate.invoke(inflater, container, false)
         return _binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vm.alertMessage.observe {
+            with(getDialog()) {
+                setDialogTitle(it.first)
+                setDialogMessage(it.second)
+                setOnPositiveClickListener { dismiss() }
+                setPositiveButtonText(R.string.component_close)
+                setCancelable(false)
+                show()
+            }
+        }
+
+        vm.navigationAction.observe {
+            navigateTo(it)
+        }
     }
 
     override fun onDestroyView() {
@@ -57,9 +85,11 @@ abstract class BaseFragment<VB : ViewBinding, VM : BaseViewModel>(
     }
 
     protected fun getDialog(): AlertDialog {
-        if(dialog == null)
+        if (dialog == null)
             dialog = AlertDialog(requireContext())
         return dialog!!
     }
+
+    protected fun navigateTo(actionId: Int) = this.findNavController().navigate(actionId)
 }
 
